4d44dfddf177727a83676b71c309af5f8fb5dccf
Andrei Karas <akaras@inbox.ru>
2018-10-30 08:25:25 +0300

    Fix checks for script files codepage.

--- .ci/checkfile.sh
+++ .ci/checkfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dos2unix "$1"
-isutf8 "$1" || exit 1
+isutf8 "$1" >>out.log
--- .ci/checkfiles.sh
+++ .ci/checkfiles.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+rm out.log
+
 find Core -type f -name "*.qs" -exec ./.ci/checkfile.sh {} \;
 find Patches -type f -name "*.qs" -exec ./.ci/checkfile.sh {} \;
 find Patches -type f -name "*.txt" -exec ./.ci/checkfile.sh {} \;
 find Addons -type f -name "*.qs" -exec ./.ci/checkfile.sh {} \;
 find Input -type f -name "*.txt" -exec ./.ci/checkfile.sh {} \;
+
+export DATA=$(cat out.log|grep -v "Input/msgStringEng.txt")
+if [[ -n "${DATA}" ]]; then
+    echo "Found wrong chars in files"
+    cat out.log|grep -v "Input/msgStringEng.txt"
+    exit 1
+fi
 
 export DATA=$(git diff)
 if [[ -n "${DATA}" ]]; then
