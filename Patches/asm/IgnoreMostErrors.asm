f1c4804872b4206247bf27c050b17917841d0bcd
Andrei Karas <akaras@inbox.ru>
2021-10-02 19:01:14 +0300

    Change license to all asm files from GPL to CC-NC-ND

--- Patches/asm/IgnoreMostErrors.asm
+++ Patches/asm/IgnoreMostErrors.asm
@@ -1,18 +1,11 @@
+; Patch (c) 2021 by Andrei Karas (4144)
 ;
-; Copyright (C) 2021  Andrei Karas (4144)
+; Patch is licensed under a
+; Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
 ;
-; Hercules is free software: you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation, either version 3 of the License, or
-; (at your option) any later version.
+; You should have received a copy of the license along with this
+; work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
 ;
-; This program is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; GNU General Public License for more details.
-;
-; You should have received a copy of the GNU General Public License
-; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 mov eax, 1
 ret 16
